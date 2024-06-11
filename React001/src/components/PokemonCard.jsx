7ee6644a5ab318a41f467bca347fd72b5dd0812d
Djo65 <joriscamus@hotmail.fr>
2024-06-11 14:09:58 +0200

    queste 3 ok

--- React001/src/components/PokemonCard.jsx
+++ React001/src/components/PokemonCard.jsx
@@ -1,12 +1,30 @@
 import  "./MyComponent.css"
 
+
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew"
+    },
+  ];
+
+
 function PokemonCard() {
+    const pokemon = pokemonList[1]
+    console.log(pokemon);
     return (
     <figure className="card">       
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="Bulbasaur" 
-         className="cardImg"/>
-        <figcaption>Bulbasaur</figcaption>       
+        {
+            pokemon.imgSrc?
+            <img src={ pokemon.imgSrc } alt="" />
+            : <p>"???"</p>
+        }
+        <figcaption>Bulbasaur</figcaption>     
     </figure>
     
 )}
